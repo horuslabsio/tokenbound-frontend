945f01a97c251cfd8d183cad784289570080511a
codeWhizperer <Adegbiteademola1999@gmail.com>
2023-12-29 09:27:27 +0100

    feat: add nonfungibe asset component

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,3 +4,5 @@ export const EtherTokenAddress: string = "0x049d36570d4e46f48e99674bd3fcc84644dd
 export const UsdcTokenAddress: string = "0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8"
 export const DaiTokenAddress: string ="0x00da114221cb83fa859dbdb4c44beeaa0bb37c7537ad5ae66fe5e0efd20e6eb3"
 export const usdtTokenAddress:string = "0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8"
+// @notice: address to test non-fungible asset for token-bound
+export const daAsset:string = "0x0696dfe9a1374f2367b133f8d41a441a826ba9da15bcd058a8407e0ef522e8b7"
