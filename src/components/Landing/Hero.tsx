e0b3f0d41c23c390f2a20e88fc52c830d68953a2
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2024-02-19 23:45:20 +0100

    Update Hero.tsx

--- src/components/Landing/Hero.tsx
+++ src/components/Landing/Hero.tsx
@@ -16,34 +16,6 @@ const Hero = () => {
       <div>
         <img src="/Frame 20.png" alt="" />
       </div>
-      {/* <div className="bg-red-700 w-screen flex h-[70vh] ">
-        
-        <svg width="200" height="100" xmlns="http://www.w3.org/2000/svg">
-          <ellipse cx="100" cy="50" rx="80" ry="40" fill="blue" />
-        </svg>
-        <svg
-          width="100%"
-          height="100%"
-          viewBox="0 0 100 70"
-          preserveAspectRatio="none"
-        >
-          <clipPath id="border">
-            <path
-              id="main-path"
-              d="M0 20 Q 100 0, 200 20 V 150 Q 150 180, 100 150 Q 50 180, 0 150 Z"
-            ></path>
-          </clipPath>
-          <image
-            clipPath="url(#border)"
-            width="100%"
-            height="100%"
-            xlinkHref="/zeus.jpg"
-            preserveAspectRatio="none"
-          ></image>
-        </svg>
-
-        
-      </div> */}
     </section>
   );
 };
