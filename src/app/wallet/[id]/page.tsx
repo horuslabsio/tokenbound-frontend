208d0d86209a6de33e3e132f1ccbcb4853c8ff5c
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2024-02-20 14:06:44 +0100

    Update page.tsx
    This PR warps the wallet page with `AppWrapper` component

--- src/app/wallet/[id]/page.tsx
+++ src/app/wallet/[id]/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import AppWrapper from "components/AppWrapper";
 import React from "react";
 import { BiCopyAlt } from "react-icons/bi";
@@ -21,32 +20,34 @@ function NFT() {
   };
 
   return (
-    <main className="min-h-screen pb-16 pt-32">
-      <div className="flex items-center space-x-4">
-        <h2 className="font-bold text-3xl">My NFTs</h2>
+    <AppWrapper>
+      <main className="min-h-screen pb-16 pt-32">
+        <div className="flex items-center space-x-4">
+          <h2 className="font-bold text-3xl">My NFTs</h2>
 
-        <p className="inline-flex items-center p-[2px] bg-gray-200 cursor-pointer rounded-full hover:transform hover:scale-110">
-          <span
-            onClick={() => copyToClipBoardHandler(address!)}
-            className="text-gray-400"
-          >
-            {address?.slice(0, 5)}...{address?.slice(60, 66)}
-          </span>
-          <span className="ml-1">
-            <BiCopyAlt />
-          </span>
-        </p>
-      </div>
+          <p className="inline-flex items-center p-[2px] bg-gray-200 cursor-pointer rounded-full hover:transform hover:scale-110">
+            <span
+              onClick={() => copyToClipBoardHandler(address!)}
+              className="text-gray-400"
+            >
+              {address?.slice(0, 5)}...{address?.slice(60, 66)}
+            </span>
+            <span className="ml-1">
+              <BiCopyAlt />
+            </span>
+          </p>
+        </div>
 
-      <div className="flex justify-start my-5 items-center">
-        <p className="mr-4 text-3xl text-gray-300">Collectibles</p>
-        <div className="border-b border-gray flex-grow"></div>
-      </div>
+        <div className="flex justify-start my-5 items-center">
+          <p className="mr-4 text-3xl text-gray-300">Collectibles</p>
+          <div className="border-b border-gray flex-grow"></div>
+        </div>
 
-      <div className="">
-        <Card />
-      </div>
-    </main>
+        <div className="">
+          <Card />
+        </div>
+      </main>
+    </AppWrapper>
   );
 }
 
