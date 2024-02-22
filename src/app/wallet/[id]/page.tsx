6dd125027de6af003e93f16ac43519e2acc035ab
ktarun1419 <tarunkaushik355@gmail.com>
2024-02-22 23:39:46 +0530

    text bold

--- src/app/wallet/[id]/page.tsx
+++ src/app/wallet/[id]/page.tsx
@@ -38,16 +38,16 @@ function NFT() {
       {isConnected && !!address ? (
         <main className="min-h-screen pb-16 pt-32">
           <div className="flex items-center space-x-4 justify-start gap-1">
-            <h2 className=" text-black text-2xl ">My NFT Collections</h2>
-            <p className="inline-flex items-center p-[4px] bg-gray-200 text-xs cursor-pointer rounded-full hover:transform hover:scale-110"  onClick={() => copyToClipBoardHandler(address!)}>
+            <h2 className=" text-black text-4xl font-medium ">My NFT Collections</h2>
+            <p className="inline-flex items-center px-[12px] py-[4px] bg-gray-200 text-sm cursor-pointer rounded-full hover:transform hover:scale-110"  onClick={() => copyToClipBoardHandler(address!)}>
               <span
                
                 className="text-gray-400"
               >
-                {address?.slice(0, 4)}...{address?.slice(62, 66)}
+                {address?.slice(0, 4)}...{address?.slice(61, 66)}
               </span>
-              <span className="ml-1 border-l border-gray-500 p-1">
-                <BiCopyAlt />
+              <span className="ml-2 border-l border-gray-500 p-1">
+                <BiCopyAlt  />
               </span>
             </p>
           </div>
