63f046271da95be3be61df6b8272a18016ac1871
Darlington02 <darlingtonnnam@gmail.com>
2024-03-27 16:43:33 +0100

    chore: fix landing page copy, update deps

--- src/components/Landing/Community.tsx
+++ src/components/Landing/Community.tsx
@@ -3,26 +3,19 @@ const Community = () => {
     <section className="bg-gradient-linear-starknet-gradient py-8 p-4 flex flex-col items-center justify-center gap-8  text-[#000000] my-0 mx-auto w-[95vw]  rounded-[20px] md:h-[50vh]">
       <h2 className="text-center">Join our Weekly Dev Call</h2>
       <p className="text-center lg:w-[40%]">
-        Learn more about Starknet Tokenbound with the builders and contributors
-        to ERC-6551 on Telegram
+        Learn more about Starknet Tokenbound with the builders and contributors on Telegram
       </p>
       <div className="flex rounded-full gap-2 bg-white  w-fit p-3 md:rounded-full md:flex-row">
         <p className="p-2 bg-off-white rounded-full text-deep-blue font-medium">
-          Every Thursday
+          Every two weeks, on Saturdays
         </p>
         <p className="p-2 bg-deep-blue rounded-full text-white font-medium">
-          9PM WAT
-        </p>
-        <p className="hidden md:flex p-2 bg-off-white rounded-full text-deep-blue font-medium">
-          9PM WAT
-        </p>
-        <p className="hidden md:flex p-2 bg-off-white rounded-full text-deep-blue font-medium">
-          9PM WAT
+          6PM WAT
         </p>
       </div>
       <a
         className="md:w-[20rem] text-center block mt-4 mx-auto p-4 bg-deep-blue rounded-[8px] text-white"
-        href="http://"
+        href="https://t.me/starknet_tokenbound"
         target="_blank"
         rel="noopener noreferrer"
       >
--- src/components/Landing/Projects.tsx
+++ src/components/Landing/Projects.tsx
@@ -56,7 +56,7 @@ const Projects = () => {
     <section className="flex flex-col gap-16  px-4 py-16 lg:p-16 lg:h-screen">
       <div className="flex flex-col lg:flex-row">
         <h2 className="text-black basis-1/2">
-          Community projects built with Starknet Tokenbound accounts
+          Community projects
         </h2>
         <div className="basis-1/2 flex justify-end items-center">
           <button className="bg-[#EFEFEF] rounded-full p-3">View All</button>
@@ -64,20 +64,20 @@ const Projects = () => {
       </div>
       <div className="flex-1 flex flex-col lg:flex-row items-center gap-20 md:gap-10">
         <Project
-          title="monieswift"
+          title="Lenstar"
           gradient="bg-gradient-to-br from-[#8DEC6B] to-[#8AB7E0]"
           description="Tokenbound Accounts turns every NFT into a smart wallet that can own
           tokens and interact with dApps across the Ethereum ecosystem"
         />
         <Project
-          title="TRAKKA"
+          title="Coming soon.."
           gradient="bg-gradient-to-br from-[#3A3A3A] to-[#282828]"
           description="Tokenbound Accounts turns every NFT into a smart wallet that can own
           tokens and interact with dApps across the Ethereum ecosystem"
         />
         <Project
-          title="monieswift"
-          src="/unsplash_hKxsoF4aubY.png"
+          title="Coming soon.."
+          gradient="bg-gradient-to-br from-[#3A3A3A] to-[#282828]"
           description="Tokenbound Accounts turns every NFT into a smart wallet that can own
           tokens and interact with dApps across the Ethereum ecosystem"
         />
